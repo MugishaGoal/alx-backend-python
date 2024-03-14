af2e43ccbdae3220ce859f82b6aa939659924367
Mugisha Goal <degaullemugisha@gmail.com>
2024-03-14 10:55:21 +0000

    Adds to floats and returns their sum

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,6 +2,7 @@
 """ a type-annotated function add that takes a float a and a float b as
 arguments and returns their sum"""
 
-def add(a: float, b:float) -> float:
+
+def add(a: float, b: float) -> float:
     """Adds two floats and returns their sum."""
     return a + b
