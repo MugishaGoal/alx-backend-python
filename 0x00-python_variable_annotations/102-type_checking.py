ec72aa2b942cd3b56b9461170f06f27f457806c0
Mugisha Goal <degaullemugisha@gmail.com>
2024-03-14 21:57:27 +0000

    Zoom in on the elements of the input list by repeating each element

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -9,7 +9,7 @@ def zoom_array(lst: Iterable, factor: int = 2) -> List:
     "Zoom in on the elements of the input list by repeating each element"
     zoomed_in: List = [
         item for item in lst
-        for i in range(factor)
+        for i in range(int(factor))
     ]
     return zoomed_in
 
@@ -18,4 +18,4 @@ array = [12, 72, 91]
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, 3)
