bf35a82c96827976361ae5d2334b090c4d8e3725
Mugisha Goal <degaullemugisha@gmail.com>
2024-03-19 17:41:37 +0000

    Coroutine that collects 10 random numbers asynchronously

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,11 +4,13 @@ over async_generator, then return the 10 random numbers"""
 
 
 import asyncio
+from typing import List
+from importlib import import_module as using
 
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """Coroutine that collects 10 random numbers asynchronously"""
     return [num async for num in async_generator()]
