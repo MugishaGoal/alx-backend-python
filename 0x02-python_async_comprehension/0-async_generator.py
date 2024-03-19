b71d011e45525aca4647d759c4afbeedc9a8bdfe
Mugisha Goal <degaullemugisha@gmail.com>
2024-03-19 17:36:23 +0000

    Coroutine that yields random numbers asynchronously

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,10 +5,11 @@ then yield a random number between 0 and 10"""
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator [float, None, None]:
     """Coroutine that yields random numbers asynchronously"""
     for _ in range(10):
         await async.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 10
