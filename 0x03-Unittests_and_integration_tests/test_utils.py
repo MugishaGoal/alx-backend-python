c72a465733c92e136333cae01c91d13cb59b9284
Mugisha Goal <degaullemmugisha@gmail.com>
2024-04-08 09:15:45 +0000

    A method to test that the method returns what it is supposed to

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -60,5 +60,38 @@ class TestGetJson(unittest.TestCase):
         self.assertEqual(result, test_payload)
 
 
+class TestMemoize(unittest.TestCase):
+    """Test case for the memoize decorator."""
+
+    class TestClass:
+        """Test class with a_method and a_property decorated with memoize."""
+
+        def a_method(self):
+            """A method to be memoized."""
+            return 42
+
+        @memoize
+        def a_property(self):
+            """A property using memoize."""
+            return self.a_method()
+
+    @patch.object(TestClass, 'a_method')
+    def test_memoize(self, mock_a_method):
+        """Test that memoization works as expected."""
+        # Create an instance of TestClass
+        test_instance = self.TestClass()
+
+        # Call the memoized property twice
+        result1 = test_instance.a_property()
+        result2 = test_instance.a_property()
+
+        # Assert that a_method was called only once
+        mock_a_method.assert_called_once()
+
+        # Assert that the results are equal
+        self.assertEqual(result1, 42)
+        self.assertEqual(result2, 42)
+
+
 if __name__ == "__main__":
     unittest.main()
