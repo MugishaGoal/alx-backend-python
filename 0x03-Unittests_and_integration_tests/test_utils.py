c356452d47e0a06b6be9643af719222bf5a70b94
Mugisha Goal <degaullemmugisha@gmail.com>
2024-04-08 09:59:28 +0000

    A Testcase for utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,7 +6,7 @@ what it is supposed to"""
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
